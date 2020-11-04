d25b855b37fa359c2ba6cdc4f71c0d2f2d204514
Jarmo Hu <34189831+JarmoHu@users.noreply.github.com>
2020-11-04 10:48:08 +0800

    Update captcha.py

--- libhustpass/captcha.py
+++ libhustpass/captcha.py
@@ -5,7 +5,7 @@ def Fuckit(imageContent):
     imageObject = Image.open(imageContent)
     imageObject.seek(1)
     grayImage = imageObject.convert("L")
-    binarizedImage = imageObject.point(lambda i: i < 255 and 255)
+    binarizedImage = grayImage.point(lambda i: i < 255 and 255)
     #binarizedImage = binarizing(imageObject, 255)
     depointedImage = binarizedImage.filter(ImageFilter.MedianFilter(5))
     code = image_to_string(depointedImage, config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
